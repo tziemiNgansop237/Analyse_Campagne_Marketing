d13f37024ed96b3e78d7bc139553ad2cdbe3b991
tziemibriand <raymondbriand.tziemingansop@ensae.fr>
2025-01-24 15:25:37 +0000

    Finalisation du projet

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Analyse_Campagne_Marketing
 L'objectif de ce projet est d'analyser le fonctionnement d'une campagne Marketing
+# tout d'abord j'ai préparé les données dans le fichier preparation_donnees.ipynb ensuite grace à des calculs explicités j'ai pu determiner des indicateurs de performances clés pour une entreprise
--- src/fonctions_utilisees.py
+++ src/fonctions_utilisees.py
@@ -22,7 +22,7 @@ def Taux_conversion(data):
     return taux
 
 def Taux_retention(data):
-        #calcul du taux de convertion global
+        #calcul du taux de retention global
         #nombre d'utilisateurs uniques
     retained = data[data['is_retained']==True]['user_id'].nunique()
     print("le nombre de personnes restés abonnés est :",retained )
@@ -32,7 +32,7 @@ def Taux_retention(data):
     print("\n")
     print("le nombre d'utilisateurs uniques ayant souscrit au forfait est :",n_souscripteurs)
 
-    #Taux de convertion global
+    #Taux de retention global
     taux_reten_global = retained/n_souscripteurs
     print("\n")
     taux = taux_reten_global*100
@@ -41,3 +41,56 @@ def Taux_retention(data):
 
     return taux
 
+
+
+#Taux de retention par canal
+
+def Taux_retention_par_variable(data,variable):
+        #calcul du taux de retention par canal
+        #nombre d'utilisateurs uniques
+    retained = data[data['is_retained']==True].groupby(variable)['user_id'].nunique()
+   # print("le nombre de personnes restés abonnés par ",variable," est :")
+   # print(retained)
+
+        #nombre d'utilisateurs uniques ayants souscrit au service par canal
+    n_souscripteurs = data[data['converted']==True].groupby(variable)['user_id'].nunique()
+    print("\n")
+   # print("le nombre d'utilisateurs uniques ayant souscrit au forfait par",variable," est :")
+    #print(n_souscripteurs)
+
+    #Taux de retention par canal
+    taux_reten_canal = retained/n_souscripteurs
+    print("\n")
+    taux = taux_reten_canal*100
+    taux.sort_values(ascending=False)
+    
+    #print("le taux de conversion global est egal à :",round(taux,2),"%")
+
+    return taux
+
+
+#taux de conversion par canal
+
+
+def Taux_conversion_par_variable(data,variable):
+        #calcul du taux de retention par canal
+        #nombre d'utilisateurs uniques
+    n_users = data.groupby(variable)['user_id'].nunique()
+   # print("le nombre de personnes restés abonnés par ",variable," est :")
+  #  print(n_users)
+
+        #nombre d'utilisateurs uniques ayants souscrit au service par canal
+    n_souscripteurs = data[data['converted']==True].groupby(variable)['user_id'].nunique()
+    print("\n")
+   # print("le nombre d'utilisateurs uniques ayant souscrit au forfait par",variable,"  est :")
+   # print(n_souscripteurs)
+
+    #Taux de retention par canal
+    taux_reten_canal = n_souscripteurs/n_users
+    print("\n")
+    taux = taux_reten_canal*100
+    taux.sort_values(ascending=False)
+    
+    #print("le taux de conversion global est egal à :",round(taux,2),"%")
+
+    return taux
